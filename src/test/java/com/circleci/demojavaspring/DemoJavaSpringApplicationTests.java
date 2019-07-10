6285d07ea07fe8af1ac83ae522ae7f23e67ba593
zerolh9 <ziroeclipse@gmail.com>
2019-07-10 16:48:04 -0700

    Update DemoJavaSpringApplicationTests.java

--- src/test/java/com/circleci/demojavaspring/DemoJavaSpringApplicationTests.java
+++ src/test/java/com/circleci/demojavaspring/DemoJavaSpringApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
